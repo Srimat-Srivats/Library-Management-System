5a55902dcca7d934565715f98225ad5e08aec0a6
SRIMAT SRIVATS <srimatsrivats7@gmail.com>
2024-12-24 19:34:54 +0530

    Add files via upload

---
+++ book.java
@@ -0,0 +1,36 @@
+package College;
+
+import java.util.Scanner;
+
+public class book {
+
+	public int sNo;
+	public String bookName;
+	public String authorName;
+	public int bookQty;
+	public int bookQtyCopy;
+
+	Scanner input = new Scanner(System.in);
+
+	// Method
+	// To add book details
+	public book()
+	{
+		// Display message for taking input later
+		// taking input via
+		// nextInt() and nextLine() standard methods
+		System.out.println("Enter Serial No of Book:");
+		this.sNo = input.nextInt();
+		input.nextLine();
+
+		System.out.println("Enter Book Name:");
+		this.bookName = input.nextLine();
+
+		System.out.println("Enter Author Name:");
+		this.authorName = input.nextLine();
+
+		System.out.println("Enter Quantity of Books:");
+		this.bookQty = input.nextInt();
+		bookQtyCopy = this.bookQty;
+	}
+}
